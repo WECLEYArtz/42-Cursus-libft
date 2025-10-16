4e561c3efad426ae472a2ec9b3035021fe798277
Ahmed Mounsif <ahmounsi@e2r11p4.1337.ma>
2025-10-16 13:13:27 +0100

    added header, testing memset()

--- ft_memset.c
+++ ft_memset.c
@@ -1,9 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahmounsi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/16 12:05:04 by ahmounsi          #+#    #+#             */
+/*   Updated: 2025/10/16 12:05:04 by ahmounsi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "stddef.h"
-void *memset(void *s, int c, size_t n){
-	while(n)
-	{
-		if (s[n])
-			s[n++] = (unsigned char)c;
-		s[n++] = 0;
-	}
+void	*ft_memset(void *dest, int c, size_t len)
+{
+	unsigned char	*dest_work;
+
+	dest_work = (unsigned char *)dest;
+	while (len-- > 0)
+		*dest_work++ = (unsigned char)c;
+	return (dest);
+}
+
+#include <stdio.h>
+#include <string.h>
+
+int	main(void)
+{
+	int		len;
+	int		offset;
+	char	test_str[100] = "The memset() function writes len bytes of value c (converted to an unsigned char) to the string b";
+	// char test_str[100] = "The memset() function writes len bytes of value c (converted to an unsigned char) to the string b";
+	char	test_target[100];
+
+	len = 10;
+	offset = 10;
+	strcpy(test_target, test_str);
+	printf("memset():\nthen:\t%s\n", test_target);
+	printf("now:\t%s\n\n",
+			(char *)memset(test_target + offset, '.', len) - offset
+			);
+	strcpy(test_target, test_str);
+	printf("ft_memset():\nthen:\t%s\n", test_target);
+	printf("now:\t%s\n",
+			(char *)ft_memset(test_target + offset, '.', len) - offset
+			);
 }
