760240aed853fffb537c630f24a8a7de897cdb1d
WECLEY <wecwhengithub7.anything491@slmail.me>
2025-10-19 23:24:50 +0100

    finito with strstrito

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -5,47 +5,49 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	size_t	i;
 	size_t	len_max;
-	size_t	haystack_len;
 	size_t	needle_len;
 
 	if (!*needle)
 		return ((char *)haystack);
-	haystack_len = ft_strlen(haystack);
 	needle_len = ft_strlen(needle);
-	if (len - needle_len >= 0) // scary
-		len_max = len - needle_len + 1;
-	if (len > len_max) // scary
-		len = len_max;
+	if (needle_len > len)
+		return (0);
+	len_max = len - needle_len + 1;
+	// if (len > len_max) // scary
+	// 	len = len_max;
 	i = 0;
-	while (len--)
+	while (len_max--)
 	{
 		if (haystack[i] == needle[0] && ft_strncmp(&haystack[i], needle,
-				len) == 0)
-			return (char *)(&haystack[i]);
+				needle_len) == 0)
+			return ((char *)(&haystack[i]));
 		i++;
 	}
 	return (NULL);
 }
 
+#include <bsd/string.h>
 #include <stdio.h>
-#include <string.h>
 
 int	main(void)
 {
 	char	*haystack;
 	char	*needle;
 
-	haystack = "test1234helloworld";
+	haystack = "test1234helloworld......";
 	needle = "1234helloworld";
-	printf("\nlib:		%s", strnstr(haystack, needle, 10));
-	printf("\nftlib:	%s", ft_strnstr(haystack, needle, 10));
+	printf("\nlib:		%s", strnstr(haystack, needle, 20));
+	printf("\nftlib:	%s", ft_strnstr(haystack, needle, 20));
 }
 
-//             V
-// [test1234helloworld], asked for :(12), len: (18)
+// [       `     ]
+// [test1234helloworld], asked for :len :(13), haystack_len: (18)
+// [       ]    `
 //        [hello],5
 
-// logically only need to search	(12 - 5 +1 ) = 8
+// ft_strnstr(needle, haystack, 13);
+
+// logically only need to search	(13 - 5 ) = 8
 // if asked to search 12 take 12
 // if asked to search 16 take 14
 // pick smallest between the two
