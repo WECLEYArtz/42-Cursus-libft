6d5cb73da6e756e598d7032fbb20fc931c61ea45
Ahmed Mounsif <ahmounsi@e3r1p4.1337.ma>
2025-10-19 14:03:38 +0100

    strnstr update

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -1,18 +1,18 @@
 #include <stddef.h>
 
-int memcmp(const void *str1, const void *str2, size_t n){
+int	memcmp(const void *str1, const void *str2, size_t n)
+{
+	unsigned char	*str1_ptr;
+	unsigned char	*str2_ptr;
 
-	size_t i = 0;
-	unsigned char *str1_ptr = (unsigned char*)str1;
-	unsigned char *str2_ptr = (unsigned char*)str1;
-
-	while ( n-- )
+	str1_ptr = (unsigned char *)str1;
+	str2_ptr = (unsigned char *)str1;
+	while (n--)
 	{
-		if ( *str1_ptr != *str2_ptr )
-			return ( *str1_ptr - *str2_ptr );
+		if (*str1_ptr != *str2_ptr)
+			return (*str1_ptr - *str2_ptr);
 		str1_ptr++;
 		str2_ptr++;
 	}
-
 	return (0);
 }
--- ft_strnstr.c
+++ ft_strnstr.c
@@ -1,9 +1,21 @@
 #include <stddef.h>
 
-char *ft_strnstr(const char *big,	const char *little, size_t len)
-{
-	while(len)
-	{
-		if(str)
-	}
+// char *ft_strnstr(const char *haystack,	const char *needle, size_t len)
+// {
+// 	while(len)
+// 	{
+// 		if(str)
+// 	}
+// }
+// [ t e s t 1 2 3 h e l l o w o r l d]
+//
+// [h e l l o] , 4
+
+
+#include <string.h>
+#include <stdio.h>
+int main(){
+	char *haystack = "test1234helloworld";
+	char *needle = "hello";
+	printf("%s",strnstr(haystack, needle,12));
 }
