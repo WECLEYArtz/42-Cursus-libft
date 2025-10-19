8cf884ebe45dd998e134bfe93c593c7a129f0ec9
Ahmed Mounsif <ahmounsi@e3r1p4.1337.ma>
2025-10-19 22:30:14 +0100

    finalising ft_strnsrt.c

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -4,26 +4,28 @@
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	size_t	i;
-	size_t	needle_len;
 	size_t	len_max;
+	size_t	haystack_len;
+	size_t	needle_len;
 
 	if (!*needle)
 		return ((char *)haystack);
-	len_max = ft_strlen(haystack) - ft_strlen(needle) +1;
+	haystack_len = ft_strlen(haystack);
+	needle_len = ft_strlen(needle);
+	if (len - needle_len >= 0) // scary
+		len_max = len - needle_len + 1;
 	if (len > len_max) // scary
 		len = len_max;
-	needle_len = ft_strlen(needle);
 	i = 0;
 	while (len--)
 	{
 		if (haystack[i] == needle[0] && ft_strncmp(&haystack[i], needle,
-				needle_len) == 0)
+				len) == 0)
 			return (char *)(&haystack[i]);
 		i++;
 	}
 	return (NULL);
 }
-
 
 #include <stdio.h>
 #include <string.h>
@@ -34,32 +36,19 @@ int	main(void)
 	char	*needle;
 
 	haystack = "test1234helloworld";
-	needle = "hello";
-	printf("\nlib:		%s", strnstr(haystack, needle, 12));
-	printf("\nftlib:	%s", ft_strnstr(haystack, needle, 12));
+	needle = "1234helloworld";
+	printf("\nlib:		%s", strnstr(haystack, needle, 10));
+	printf("\nftlib:	%s", ft_strnstr(haystack, needle, 10));
 }
 
-// [test1234helloworld],(12)18
-//              [hello],5
+//             V
+// [test1234helloworld], asked for :(12), len: (18)
+//        [hello],5
 
-// if asked to search 16
-// logically only need to search	(18 - 5 + 1) = 14
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// logically only need to search	(12 - 5 +1 ) = 8
+// if asked to search 12 take 12
+// if asked to search 16 take 14
+// pick smallest between the two
 
 // [test123helloworld],17 haystack
 //      [123hellworld],12
