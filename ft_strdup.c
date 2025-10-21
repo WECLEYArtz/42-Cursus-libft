7187a300585b87e6e9db06d3a67eb9dc5f14d840
WECLEY <wecwhengithub7.anything491@slmail.me>
2025-10-21 12:31:31 +0100

    what

--- ft_strdup.c
+++ ft_strdup.c
@@ -1,27 +1,28 @@
-// #include "libft.h"
-// #include <stdlib.h>
-//
-// static	void ft_strcpy(char *dst, const char *src){
-// 	while(*src)
-// 		*dst++ = *src++;
-// 	*dst = 0;
-// }
-//
-// char *ft_strdup(const char *str){
-// 	size_t str_len;
-//
-// 	str_len = ft_strlen(str);
-// 	char  *new_str = malloc(str_len +1);
-// 	if(!new_str)
-// 		return 0;
-// 	ft_strcpy(new_str, str);
-// 	return  (new_str);
-// }
+#include "libft.h"
+#include <stdlib.h>
+
+static	void ft_strcpy(char *dst, const char *src){
+	while(*src)
+		*dst++ = *src++;
+	*dst = 0;
+}
+
+char *ft_strdup(const char *str){
+	size_t str_len;
+
+	str_len = ft_strlen(str);
+	char  *new_str = malloc(str_len +1);
+	if(!new_str)
+		return 0;
+	ft_strcpy(new_str, str);
+	return  (new_str);
+}
 
 #include <stdio.h>
 #include <string.h>
 int main(){
 	char *src = "";
-	char *test = strdup(src);
+	char *test;
+	test = strdup(0);
 	printf("balls : %s",test);
 }
