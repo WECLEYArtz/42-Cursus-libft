8f322cabbd08435e649e117e3aac284a20c6d4ac
WECLEY <wecwhengithub7.anything491@slmail.me>
2025-10-21 12:05:47 +0100

    first push in dev, stdrdup working on

--- libft.h
+++ libft.h
@@ -27,5 +27,6 @@ char	*ft_strrchr(const char *str, int chr);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 int		ft_atoi(const char *str);
+char *ft_strdup(const char *s);
 void	*calloc(size_t count, size_t size);
 #endif
--- todo.md
+++ todo.md
@@ -20,5 +20,5 @@
 <!-- • strnstr -->
 <!-- • atoi -->
 
-• calloc
+<!-- • calloc -->
 • strdup
