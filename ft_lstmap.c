3ab37163e262b9aee10729aa68742c71aa93a3aa
WECLEY <wecwhengithub7.anything491@slmail.me>
2025-10-27 18:17:41 +0100

    new ft_lstmap function

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -12,6 +12,37 @@
 
 #include "libft.h"
 
+static void	*ft_lstabort(t_list **lst, void (*del)(void *))
+{
+	ft_lstclear(lst, del);
+	return (NULL);
+}
+
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
+	t_list	*newlst;
+	t_list	*current;
+
+	if (!lst)
+		return (NULL);
+	newlst = ft_lstnew(f(lst->content));
+	if (!newlst)
+		return (ft_lstabort(&lst, del));
+	current = newlst;
+	lst = lst->next;
+	while (lst)
+	{
+		current->next = ft_lstnew(f(lst->content));
+		if (!current->next)
+			return (ft_lstabort(&lst, del));
+		current = current->next;
+		lst = lst->next;
+	}
+	return (current);
 }
+
+// [[content][next]]  _[[content][next]]   _[[content][next]]   _[[content][next]]
+//               \___/                \___/                \___/
+//
+// [[content][next]]  _[[content][next]]   _[[content][next]]   _[[content][next]]
+//               \___/                \___/                \___/
