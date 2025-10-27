c43ab8e31db62b24306f2c89780eb7f532e3ed37
WECLEY <wecwhengithub7.anything491@slmail.me>
2025-10-27 22:32:51 +0100

    corrected bonus naming

---
+++ ft_lstclear_bonus.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear_bonus.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahmounsi <<marvin@42.fr>>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/27 03:07:31 by ahmounsi          #+#    #+#             */
+/*   Updated: 2025/10/27 22:30:34 by ahmounsi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*next;
+
+	if (!lst || !(*lst))
+		return ;
+	while (next)
+	{
+		next = (*lst)->next;
+		ft_lstdelone(*lst, del);
+		*lst = next;
+	}
+}
---
+++ ft_lstmap_bonus.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap_bonus.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahmounsi <<marvin@42.fr>>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/27 04:31:04 by ahmounsi          #+#    #+#             */
+/*   Updated: 2025/10/27 22:30:34 by ahmounsi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static void	*ft_lstabort(t_list **lst, void (*del)(void *))
+{
+	ft_lstclear(lst, del);
+	return (NULL);
+}
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*newlst;
+	t_list	*current;
+
+	if (!lst)
+		return (NULL);
+	newlst = ft_lstnew(f(lst->content));
+	if (!newlst)
+		return (ft_lstabort(&lst, del));
+	current = newlst;
+	lst = lst->next;
+	while (lst)
+	{
+		current->next = ft_lstnew(f(lst->content));
+		if (!current->next)
+			return (ft_lstabort(&lst, del));
+		current = current->next;
+		lst = lst->next;
+	}
+	return (current);
+}
+
+// [[content][next]]  _[[content][next]]   _[[content][next]]   _[[content][next]]
+//               \___/                \___/                \___/
+//
+// [[content][next]]  _[[content][next]]   _[[content][next]]   _[[content][next]]
+//               \___/                \___/                \___/
