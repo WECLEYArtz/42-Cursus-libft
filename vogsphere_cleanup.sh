21df57b64de6de5129c4ff0bac2b2a3cf37df1db
Ahmed Mounsif <ahmounsi@e3r1p4.1337.ma>
2025-11-01 02:32:01 +0100

    attempt 2

--- vogsphere_cleanup.sh
+++ vogsphere_cleanup.sh
@@ -1,5 +1,12 @@
+
+git checkout -b vogsphere
+
 rm -rf libft.a
 rm -rf .tests
 rm -rf *.o
 rm -rf todo.md
 rm -rf ./vogsphere_cleanup.sh
+
+git add .
+git commit -m "auto delete"
+git push 42 vogsphere:main
