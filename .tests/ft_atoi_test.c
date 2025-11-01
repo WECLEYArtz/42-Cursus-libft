bb995b465ef692a8953aaa40149e2ab20ebdcc54
Ahmed Mounsif <ahmounsi@e3r1p4.1337.ma>
2025-11-01 19:28:23 +0100

    weird aaah test

--- .tests/ft_atoi_test.c
+++ .tests/ft_atoi_test.c
@@ -6,34 +6,27 @@ int	main(void)
 {
 	char	*test;
 
-	// test = "-0"; // the uh
-	// test = "0"; // the uh
-	// test = "1"; // the uh
-	test = "+1"; // the uh
-	// test = "-2147483648"; // int min
-	// test = "2147483647"; // int max
-	// test = "-2147483649"; // overflow to int max
-	// test = "-2147483648"; // overflow to int max
-	// test = " r\n\r\v\f\t-1993acbd"; // random dogshit
-	// test = "2147483648"; // overflow to int max
-
-	// test = "922337203685477580"; // long long max - digit;
-	// test = "9223372036854775807"; // long long max
-	// test = "-9223372036854775808"; // long long min
-	// test = " 9223372036854775808"; // long long max + 1	has to be -1;
-	// test = "-9223372036854775809"; // long long min - 1		 has to be 0;
-
-
-	// test = " 9999999999999999999"; // long long max + allot	 has to be -1;
-	// test = "-9999999999999999999"; // long long min - allot		 has to be 0;
-
-	// printf("\natoi:	%d", atoi(test));
-	// printf("\nft_atoi:	%d\n", ft_atoi(test));
-
+	printf("%s\n","[tests]"); // the uh
+	printf("huh %d\n",ft_atoi("-0")); // the uh
+	printf("%d\n",ft_atoi("0")); // the uh
+	printf("%d\n",ft_atoi("1")); // the uh
+	printf("%d\n",ft_atoi("+1")); // the uh
+	printf("%d\n",ft_atoi("-2147483648")); // int min
+	printf("%d\n",ft_atoi("2147483647")); // int max
+	printf("%d\n",ft_atoi("-2147483649")); // overflow to int max
+	printf("%d\n",ft_atoi("-2147483648")); // overflow to int max
+	printf("%d\n",ft_atoi(" r\n\r\v\f\t-1993acbd")); // random dogshit
+	printf("%d\n",ft_atoi("2147483648")); // overflow to int max
+	printf("%d\n",ft_atoi("922337203685477580")); // long long max - digit;
+	printf("%d\n",ft_atoi("9223372036854775807")); // long long max
+	printf("%d\n",ft_atoi("-9223372036854775808")); // long long min
+	printf("%d\n",ft_atoi(" 9223372036854775808")); // long long max + 1	has to be -1;
+	printf("%d\n",ft_atoi("-9223372036854775809")); // long long min - 1		 has to be 0;
+	printf("%d\n",ft_atoi(" 9999999999999999999")); // long long max + allot	 has to be -1;
+	printf("%d\n",ft_atoi("-9999999999999999999")); // long long min - allot		 has to be 0;
 	printf("%d\n",ft_atoi("+0000000000000000000000000000000000000000000000000000123"));
-	printf("%d\n",atoi("+0000000000000000000000000000000000000000000000000000123"));
-
+	printf("%d\n",ft_atoi("+0000000000000000000000000000000000000000000000000000123"));
 }
 
 // NEOVIM
-// set mp=cc\ %\ ../libft.a\ &&\ ./a.out
+// set mp=make\ -C\ ../\ CC='cc\ -g'\ &&\ cc\ -g\ %\ ../libft.a\ &&\ ./a.out
