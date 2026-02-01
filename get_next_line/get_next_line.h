8b740acf88ed7330b8e6967d6d57402b764167f6
Ahmed Mounsif <ahmounsi@e1r7p9.1337.ma>
2026-02-01 09:54:06 +0100

    uneeded libraries

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -13,10 +13,8 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include <stddef.h>
 # include <stdlib.h>
 # include <unistd.h>
-# include <limits.h>
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 69
